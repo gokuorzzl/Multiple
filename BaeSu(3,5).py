a85075d813ceec5099ec738d4dc9b92388b140d6
Kim Myung Sang <gokuorzzl@naver.com>
2015-07-13 20:04:16 +0900

    add funtion

--- BaeSu(3,5).py
+++ BaeSu(3,5).py
@@ -1,9 +1,17 @@
 def sum_3_or_5(n):
+    result = 0
     for i in range(2, n+1):
-        pass
+        if i % 3 == 0:
+            result += i
+        if i % 5 == 0:
+            result += i
+        if i % 3 == 0 and i % 5 == 0:
+            result -= i
+    return result
+
 
 def main():
-    n=input('n보다 작은 3 or 5의 배수를 모두 더하면?')
+    n = input('n보다 작은 3 or 5의 배수를 모두 더하면?')
     n = int(n)
     print('3과 5의 배수를 모두 더한값:', sum_3_or_5(n))
 
